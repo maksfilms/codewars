9f23b09364f2fb37ee02a8c4a694302e7ad349c2
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-06-16 23:06:04 +0200

    solution of the task

---
+++ convert-boolean-to-string.js
@@ -0,0 +1,8 @@
+function booleanToString(b){
+    return (b) ? 'true' : 'false';
+}
+
+/*
+Implement a function which convert the given boolean value into its string representation.
+
+    Note: Only valid inputs will be given.*/
