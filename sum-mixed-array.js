d680aabc183a44b464dbbdf310df5423c6895c51
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-06-30 23:06:21 +0300

    solution of the task

---
+++ sum-mixed-array.js
@@ -0,0 +1,13 @@
+function sumMix(x) {
+    let sum = 0;
+
+    for (let i = 0; i < x.length; i++) {
+        sum += Number(x[i]);
+    }
+    return sum;
+}
+
+/*
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+    Return your answer as a number.*/
